15ffa2c096032d7748a8d6b130549c9cfbc2aae9
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-01-14 14:19:03 +0900

    fix: restrict by ip range

--- src/index.php
+++ src/index.php
@@ -1,8 +1,3 @@
-<?php
-const VALID_SOURCE_IP = "192.168.0.10";
-$FLAG = $_ENV["FLAG"];
-?>
-
 <html>
 <head>
     <title>internal-only</title>
@@ -15,14 +10,16 @@ if (array_key_exists("HTTP_X_FORWARDED_FOR", $_SERVER)) {
     $source_ips = explode(",", $_SERVER["HTTP_X_FORWARDED_FOR"]);
     $trimmed = array_map(fn($x) => trim($x), $source_ips);
     $client_ip = $trimmed[0];
+    $split = explode(".", $client_ip);
+    $client_ip_prefix = join(".", array_slice($split, 0, 3));
 
-    if ($client_ip === VALID_SOURCE_IP) {
+    if ($client_ip_prefix === "192.168.0") {
         $valid = true;
     }
 }
 
 if ($valid) {
-    echo "flag:$FLAG", PHP_EOL;
+    echo "flag:this_is_flag", PHP_EOL;
 } else {
     echo "Forbidden", PHP_EOL;
 }
